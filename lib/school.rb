7a6013f943dd0e5172d3600114e5f28b7a8cf149
Harrison Blake <harrison.r.blake@gmail.com>
2020-12-02 13:12:20 -0700

    finished iteration 1 tests and methods

--- lib/school.rb
+++ lib/school.rb
@@ -1,9 +1,9 @@
 class School
   attr_reader :hours_in_school_day, :start_time, :student_names
 
-  def initialize(hours_in_day, start)
-    @hours_in_school_day = hours_in_day
-    @start_time = start
+  def initialize(start_time, hours_in_school_day)
+    @hours_in_school_day = hours_in_school_day
+    @start_time = start_time
     @student_names = []
   end
 end
--- test/school_test.rb
+++ test/school_test.rb
@@ -5,28 +5,28 @@ require './lib/school'
 class SchoolTest < Minitest::Test
   #Iteration 1 Tests:
   def test_it_exists
-    skip
+    #skip
     school = School.new('9:00', 7)
 
     assert_instance_of School, school
   end
 
   def test_it_has_start_time
-    skip
+    #skip
     school = School.new('9:00', 7)
 
     assert_equal '9:00', school.start_time
   end
 
   def test_it_has_hours_in_school_day
-    skip
+    #skip
     school = School.new('9:00', 7)
 
     assert_equal 7, school.hours_in_school_day
   end
 
   def test_it_starts_with_no_student_names
-    skip
+    #skip
     school = School.new('9:00', 7)
 
     assert_equal [], school.student_names
