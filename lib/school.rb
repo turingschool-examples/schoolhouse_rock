accc002d3c082b041b899749ef3c4c8ea9acab97
Andrew Smith <andrewpatrick138@gmail.com>
2020-12-02 15:57:46 -0800

    add school

---
+++ lib/school.rb
@@ -0,0 +1,21 @@
+class School
+    def initialize(time, hours)
+      @start_time = time
+      @hours = hours
+      @student_names = []
+    end
+    def add_student_name(student_name)
+        @student_names.push
+  end
+
+school = School.new('9:00', 7,)
+
+#school.hours_in_school_day
+
+#school.student_names
+
+#school.add_student_name('Aurora')
+#school.add_student_name('tim')
+#school.add_student_name('megan')
+
+##school.student_names
