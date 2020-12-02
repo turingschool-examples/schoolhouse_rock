f8c89d9c8f753214e4610af1922305e0d5960af8
Trevor Suter <Trevorsuter@icloud.com>
2020-12-02 13:12:22 -0700

    test complete up to starts with no names

--- lib/school.rb
+++ lib/school.rb
@@ -6,5 +6,9 @@ attr_reader :start_time, :hours_in_school_day
     @start_time = start_time
     @hours_in_school_day = hours_in_school_day
   end
+
+  def student_names
+    student_names = []
+  end
   
 end
