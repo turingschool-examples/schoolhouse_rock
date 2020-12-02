35de277e856e852b39118bd88421cb49ed100d37
Alex Schwartz <aschwartz@detaso.com>
2020-12-02 14:13:15 -0600

    Get first few iteration 1 tests passing

---
+++ lib/school.rb
@@ -0,0 +1,10 @@
+class School
+  attr_reader :start_time,
+              :hours_in_school_day
+
+  def initialize(start_time, hours_in_school_day)
+    @start_time = start_time
+    @hours_in_school_day = hours_in_school_day
+    @students = []
+  end
+end
