8522682cf32917ac8ff052adca32304cdce5f90f
Brisa Garcia <brisagarciaglz@gmail.com>
2020-12-02 13:32:50 -0700

    update

--- lib/school.rb
+++ lib/school.rb
@@ -1,6 +1,8 @@
 class SchoolTest
-  attr_reader :school,
-  def initialize(school)
-    @school = school
+  attr_reader :start_time,
+              :hours
+  def initialize(start_time, hours)
+    @start_time = start_time
+    @hours = hours
 end
 end
