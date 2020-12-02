857d34687475ebefe57d5d5f825e7dc44221bae3
Jeremiah Michlitsch <jeremiahmichlitsch@gmail.com>
2020-12-02 14:09:20 -0700

    First Iteration

---
+++ test/school.rb
@@ -0,0 +1,16 @@
+class School
+  def initialize(start_time,hours_in_school_day)
+    def start_time
+    start_time = "9:00"
+    end
+    def hours_in_school_day
+      hours_in_school_day = ()
+    end
+    def student_names
+      student_names = []
+    end
+    def end_time
+    shc
+    end
+  end
+end
