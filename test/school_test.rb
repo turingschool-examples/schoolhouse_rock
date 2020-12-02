0134107e2964cd73d5048f8c4269da80c3b00a22
Jacob Arellano <jmm.arellano@gmail.com>
2020-12-02 13:28:04 -0700

    add test, unskip

--- test/school_test.rb
+++ test/school_test.rb
@@ -5,28 +5,28 @@ require './lib/school'
 class SchoolTest < Minitest::Test
   #Iteration 1 Tests:
   def test_it_exists
-    skip
+
     school = School.new('9:00', 7)
 
     assert_instance_of School, school
   end
 
   def test_it_has_start_time
-    skip
+
     school = School.new('9:00', 7)
 
     assert_equal '9:00', school.start_time
   end
 
   def test_it_has_hours_in_school_day
-    skip
+
     school = School.new('9:00', 7)
 
     assert_equal 7, school.hours_in_school_day
   end
 
   def test_it_starts_with_no_student_names
-    skip
+
     school = School.new('9:00', 7)
 
     assert_equal [], school.student_names
@@ -34,7 +34,7 @@ class SchoolTest < Minitest::Test
 
   #Iteration 2 Tests:
   def test_it_can_add_student_names
-    skip
+
     school = School.new('9:00', 7)
 
     school.add_student_name('Aurora')
@@ -45,11 +45,22 @@ class SchoolTest < Minitest::Test
   end
 
   def test_it_can_calculate_end_time
-    skip
+
     school1 = School.new('9:00', 7)
     school2 = School.new('9:00', 3)
+    school3 = School.new('7:00', 3)
 
     assert_equal '16:00', school1.end_time
     assert_equal '12:00', school2.end_time
+    assert_equal '10:00', school3.end_time
+  end
+
+  def test_school_is_full_time
+
+    school1 = School.new('6:00', 3)
+    school2 = School.new('10:00', 4)
+
+    refute school1.is_full_time?
+    assert school2.is_full_time?
   end
 end
