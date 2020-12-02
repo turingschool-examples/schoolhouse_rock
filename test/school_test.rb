8c3e0c243e91fd1b0662c7f992a29d8a910e9699
Trevor Robinson <trevor.robi1254@gmail.com>
2020-12-02 13:41:26 -0700

    Create standard_student_names test

--- test/school_test.rb
+++ test/school_test.rb
@@ -59,5 +59,14 @@ class SchoolTest < Minitest::Test
     assert_equal true, school.is_full_time?
   end
 
-  
+  def test_it_can_give_standard_student_names
+    school = School.new('9:00', 7)
+
+    school.add_student_name('Aurora')
+    school.add_student_name('tim')
+    school.add_student_name('megan')
+
+    assert_equal ["Aurora", "Tim", "Megan"], school.standard_student_names 
+
+  end
 end
