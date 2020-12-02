d57228dcb24440422a69923c78276e81b821098d
Arika Turing <ablenker@gmail.com>
2020-12-02 13:19:08 -0700

    Complete test 3

--- lib/school.rb
+++ lib/school.rb
@@ -1,9 +1,9 @@
 class School
   attr_reader :start_time,
-               :hours_in_class
-               
-  def initialize(start_time, hours_in_class)
+               :hours_in_school_day
+
+  def initialize(start_time, hours_in_school_day)
     @start_time = start_time
-    @hours_in_class = hours_in_class
+    @hours_in_school_day = hours_in_school_day
   end
 end
