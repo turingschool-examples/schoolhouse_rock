551ace928e0fa5c3a73e218e3193f5b8c5f0a95a
Joseph Budina <joebud3@gmail.com>
2020-12-02 13:32:13 -0700

    Add convert clock test

--- test/school_test.rb
+++ test/school_test.rb
@@ -70,4 +70,10 @@ class SchoolTest < Minitest::Test
 
     assert_equal expected, school.standard_student_names
   end
+
+  def test_it_can_convert_end_time_to_clock_time
+    school = School.new('9:00', 7)
+
+    assert_equal "4:00", school.convert_end_time_to_clock_time
+  end
 end
