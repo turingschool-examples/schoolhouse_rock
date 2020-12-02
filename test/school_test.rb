d946a899983f00aa2032e1c140e8e5d0c61e3c42
Jesus Quezada-Guillen <maj3st1@gmail.com>
2020-12-02 14:37:24 -0600

    Add test for convert_end_time_to_clock_time method of School class.

--- test/school_test.rb
+++ test/school_test.rb
@@ -66,4 +66,9 @@ class SchoolTest < Minitest::Test
 
     assert_equal ["Aurora", "Tim", "Megan"], school.standard_student_names
   end
+
+  def test_it_can_convert_time_format
+    school = School.new('9:00', 7)
+    assert_equal "4:00", school.convert_end_time_to_clock_time
+  end
 end
