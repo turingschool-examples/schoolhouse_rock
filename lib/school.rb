9c9369d39b86dba8b90a6e6155f972a11839049a
Jennifer Brabson <jenniferbrabson@Jennifers-MacBook-Pro.local>
2020-12-02 14:28:07 -0700

    Updates on school.rb

--- lib/school.rb
+++ lib/school.rb
@@ -1,8 +1,21 @@
 class School
   attr_reader :start_time, :hours_in_school_day
+
   def initialize(start_time, hours_in_school_day)
     @start_time          = start_time
     @hours_in_school_day = hours_in_school_day
   end
-   
+
+  def student_names
+    student_names = student_names.new()
+  end
+
+  def add_student_name
+    student_names {'Aurora' 'tim' 'megan'}
+
+  def calculate_end_time(start_time, hours_in_school_day)
+    end_time = start_time + hours_in_school_day
+  end
+
+
 end
