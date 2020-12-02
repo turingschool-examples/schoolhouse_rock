da5b2e1be49a4010f6f44e8f3fb3e661c761cffe
Caryn Keffer <carynkeffer@gmail.com>
2020-12-02 15:36:45 -0500

    Initial commit

---
+++ lib/school.rb
@@ -0,0 +1,48 @@
+class School
+  attr_reader :start_time,
+              :hours_in_school_day,
+              :student_names
+  def initialize(start_time, hours_in_school_day)
+    @start_time          = start_time
+    @hours_in_school_day = hours_in_school_day
+    @student_names       = []
+  end
+
+  def add_student_name(name)
+    @student_names << name
+  end
+
+  def military_time
+    start_time[0].to_i + hours_in_school_day
+  end
+
+  def end_time
+    military_time.to_s + ":00"
+  end
+
+  def is_full_time?
+    if @hours_in_school_day >= 4
+      true
+    else
+      false
+    end
+  end
+
+  def standard_student_names
+    student_names.map do |name|
+      name.capitalize
+    end
+  end
+
+  def clock_time
+    if military_time >= 13
+      military_time - 12
+    else
+      end_time
+    end
+  end
+
+  def convert_end_time_to_clock_time
+    clock_time.to_s + ":00"
+  end
+end
