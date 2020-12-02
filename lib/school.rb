abb9745e75d05431fc6a04fb115b0edff1f2393c
Brisa Garcia <brisagarciaglz@gmail.com>
2020-12-02 13:50:53 -0700

    lib file

--- lib/school.rb
+++ lib/school.rb
@@ -1,8 +1,22 @@
 class School
   attr_reader :start_time,
-              :hours
+              :hours,
+              :student_name
   def initialize(start_time, hours)
     @start_time = start_time
     @hours = hours
+    @student_names = student_names []
+
 end
+
+def start_time (start_time, hours)
+  @start_time = start_time
+end
+
+def hours_in_school_day(start_time, hours)
+  @hours
+end
+
+def student_names(student_name)
+  @student_name = student_name
 end
