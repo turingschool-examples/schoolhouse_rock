a8589e5509f0b8fe58e0af57bd88f28746cdec14
Alex Schwartz <aschwartz@detaso.com>
2020-12-02 14:43:48 -0600

    Create end time calculation for School

--- lib/school.rb
+++ lib/school.rb
@@ -12,4 +12,18 @@ class School
   def add_student_name(name)
     @student_names.push(name)
   end
+
+  def end_time
+    # Separate the time on a colon
+    split_start_time = @start_time.split(':')
+
+    # Convert start
+    start_hour = split_start_time[0].to_i
+
+    # Calculate end hour
+    end_hour = start_hour + @hours_in_school_day
+
+    # Create end time string
+    end_time = "#{end_hour}:#{split_start_time[1]}"
+  end
 end
