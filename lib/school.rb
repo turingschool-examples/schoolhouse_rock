36263731314a691a6905b6df88711b6e1a9ba1db
Diana Buffone <diana.pamelaet@gmail.com>
2020-12-02 15:15:25 -0500

    Initial commit

---
+++ lib/school.rb
@@ -0,0 +1,16 @@
+class School
+  def initialize(start_time, hours_in_school_day)
+  end
+
+  def start_time
+    @start_time = '9:00'
+  end
+
+  def hours_in_school_day
+    @hours_in_school_day = 7
+  end
+
+  def student_names
+    @student_names = []
+  end
+end
