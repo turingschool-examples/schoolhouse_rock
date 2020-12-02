7a4088189eb0b15017656dee389e9662019638c1
Jacob Arellano <jmm.arellano@gmail.com>
2020-12-02 14:26:12 -0700

    remove excess in initialize

--- lib/school.rb
+++ lib/school.rb
@@ -1,9 +1,10 @@
 class School
   attr_reader :start_time, :hours_in_school_day, :student_names
-  def initialize(start_time, hours_in_school_day, student_names=[])
+  def initialize(start_time, hours_in_school_day)
     @start_time = start_time
     @hours_in_school_day = hours_in_school_day
-    @student_names = student_names
+    @student_names = []
+    
   end
 
   def add_student_name(name)
