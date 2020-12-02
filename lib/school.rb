720e6a2edea165b07ef8dad3424c7103fc80760e
DougWelchons <doug.welchons@gmail.com>
2020-12-02 13:39:21 -0700

    Add passing School class 'standard_student_names'.

--- lib/school.rb
+++ lib/school.rb
@@ -17,7 +17,14 @@ class School
     "#{end_time}:00"
   end
 
-  def full_time?
+  def is_full_time?
     @hours_in_school_day > 4
   end
+
+  def standard_student_names
+    @student_names.map! do |name|
+      name.capitalize
+    end
+  end
+
 end
