1b894ccecbbd783031c0826bb8372f63b7a4506c
Jennifer Brabson <jenniferbrabson@Jennifers-MacBook-Pro.local>
2020-12-02 13:54:04 -0700

    Initial commit

---
+++ lib/school.rb
@@ -0,0 +1,8 @@
+class School
+  attr_reader :start_time, :hours_in_school_day
+  def initialize(start_time, hours_in_school_day)
+    @start_time          = start_time
+    @hours_in_school_day = hours_in_school_day
+  end
+   
+end
