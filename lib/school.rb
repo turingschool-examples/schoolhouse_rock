3bc1a787625fdee52c01a82bfab3c8f3c0d64786
Khoa Nguyen <KhoaNg323@hotmail.com>
2020-12-02 13:53:48 -0700

    Added iteration2 stuff

--- lib/school.rb
+++ lib/school.rb
@@ -1,11 +1,24 @@
+require 'time'
+
 class School
  attr_reader :hours_in_school_day,
-             :start_time,
+             :student_names,
+             :start_time
 
-  def initialize(attributes)
-    @hours_in_school_day
-    @start_time
+  def initialize(hours_in_school_day, start_time)
+    @hours_in_school_day = hours_in_school_day
+    @start_time = start_time
     @student_names = []
+    @is_full_time? = true
   end
 
+  def add_student_name(x)
+    @student_names << x
+  end
+
+  def end_time
+  etime = (hours_in_school_day.to_i + start_time.to_i)
+  Time.strptime(etime, "%I%P").strftime("%H:%M")
+
+  end
 end
