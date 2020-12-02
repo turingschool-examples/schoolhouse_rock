8102879243205cf54b72b99b44e246cb2d3b86dd
Tommy Nieuwenhuis <thomas.nieuwenhuis24@gmail.com>
2020-12-02 13:23:24 -0700

    Complete iteration 2

--- lib/school.rb
+++ lib/school.rb
@@ -10,4 +10,18 @@ attr_reader :start_time,
     @student_names = []
   end
 
+  def add_student_name(name)
+    student_names << name
+  end
+
+  def end_time
+  no_colon = @start_time.delete":"
+  to_integer = no_colon.to_i
+  start_hour = to_integer/100
+  end_hour = start_hour + @hours_in_school_day
+  with_zeros = end_hour * 100
+  to_string = with_zeros.to_s
+  end_time = to_string.insert(-3, ":")
+  end
+
 end
